ca92e8d90e8ed222afd0a97656928dfcc3c4f52b
traplordpanda <kdaub.tf2@gmail.com>
2024-05-06 19:39:01 -0400

    initial commit

---
+++ source/lib.cpp
@@ -0,0 +1,272 @@
+#include <bit>
+#include <stdexcept>
+
+#include "lib.hpp"
+
+#include <fmt/core.h>
+
+constexpr bit_field value_mask = 0b111111111;
+
+Cell::Cell() noexcept
+    : data(0)
+{
+}
+
+Cell::Cell(char c) noexcept
+    : data(static_cast<bit_field>(1 << (c - '0' - 1)))
+{
+}
+
+Cell::Cell(bit_field bits) noexcept
+    : data(bits)
+{
+}
+
+Cell Cell::operator|(const Cell& other) const
+{
+  return Cell(static_cast<bit_field>(data | other.data));
+}
+
+Cell Cell::operator&(const Cell& other) const
+{
+  return Cell(static_cast<bit_field>(data & other.data));
+}
+
+Cell& Cell::operator|=(const Cell& other)
+{
+  data |= other.data;
+  return *this;
+}
+
+Cell& Cell::operator=(const Cell& other)
+{
+  if (this != &other) {
+    data = other.data;
+  }
+  return *this;
+}
+
+auto Cell::is_set() const noexcept -> bool
+{
+  return std::has_single_bit(data);
+}
+
+constexpr auto Cell::to_char() const noexcept -> char
+{
+  if (data == 0 || std::popcount(data) != 1) {
+    return '0';
+  }
+  int bit_position = std::countr_zero(data);
+  return '0' + bit_position + 1;
+}
+
+Bitdoku::Bitdoku() noexcept {};
+
+Bitdoku::Bitdoku(const std::string& board)
+{
+  if (board.size() != flat_board_size) {
+    throw std::runtime_error(
+        "Invalid board size. Must be exactly 81 characters.");
+  }
+  for (int i = 0; i < flat_board_size; ++i) {
+    if (board[i] >= '1' && board[i] <= '9') {  // '0' represents an empty cell
+      bitboards[i] = board[i];
+    }
+  }
+}
+
+const auto Bitdoku::get_board() const
+{
+  return bitboards;
+}
+
+auto Bitdoku::set(int row, int col, int num) noexcept -> void 
+{
+  bitboards[index_row_col(row, col)] = static_cast<bit_field>(1 << num);
+}
+
+auto Bitdoku::set(int index, bit_field value) noexcept -> void 
+{
+  bitboards[index] = value;
+}
+
+auto Bitdoku::is_valid_row(const int index) const -> bool
+{
+  auto possible = get_row(index);
+  return !(possible ^ expected_mask);
+}
+
+auto Bitdoku::is_valid_col(const int index) const -> bool
+{
+  auto possible = get_col(index);
+  return !(possible ^ expected_mask);
+}
+
+auto Bitdoku::is_valid_block(const int index) const -> bool
+{
+  auto possible = get_block(index);
+  return !(possible ^ expected_mask);
+}
+
+auto Bitdoku::is_valid() const -> bool
+{
+  for (int i = 0; i < 9; i++) {
+    if (!is_valid_row(i) || !is_valid_col(i) || !is_valid_block(i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+auto Bitdoku::set_possible(const std::size_t board_index) -> bool
+{
+  // early return if already set
+  if (bitboards[board_index].is_set()) {
+    return true;
+  }
+
+  const auto [col, row] = find_col_row(board_index);
+  const auto block = (row / 3) * 3 + (col / 3);
+  bit_field used = get_row(row) | get_col(col) | get_block(block);
+  bit_field possible = ~used & expected_mask;
+
+  if (possible == 0) {
+    return false;  // No possible values, invalid state
+  }
+
+  // board will be set with the possible values
+  bitboards[board_index] = possible;
+  return true;
+}
+
+auto Bitdoku::find_empty_cell() const -> int
+{
+  for (int i = 0; i < flat_board_size; i++) {
+    if (!bitboards[i].is_set()) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+auto Bitdoku::is_valid_move(const int board_index,
+                            const bit_field num_bit) const -> bool 
+{
+  const auto [col, row] = find_col_row(board_index);
+  const auto block = (row / 3) * 3 + (col / 3);
+
+  bit_field used = get_row(row) | get_col(col) | get_block(block);
+  return !(used & num_bit);
+}
+
+auto Bitdoku::solve() -> bool
+{
+  int empty_cell = find_empty_cell();
+  if (empty_cell == -1) {
+    return true;  // All cells are filled, puzzle is solved
+  }
+
+  for (int num = 1; num <= 9; ++num) {
+    bit_field num_bit = 1 << (num - 1);
+    if (is_valid_move(empty_cell, num_bit)) {
+      set(empty_cell, num_bit);
+
+      if (set_possible(empty_cell) && solve()) {
+        return true;
+      }
+
+      // Backtrack
+      set(empty_cell, 0);
+    }
+  }
+
+  return false;  // No valid solution found
+}
+
+auto Bitdoku::print_board_bits() const -> void
+{
+  for (std::size_t i = 0; i < flat_board_size; i++) {
+    fmt::print("{:b} ", bitboards[i].data);
+    if ((i + 1) % 9 == 0) {
+      fmt::print("\n");
+    }
+  }
+}
+
+auto Bitdoku::to_string() const noexcept -> std::string 
+{
+  std::string board;
+  board.reserve(flat_board_size);
+  for (auto& cell : bitboards) {
+    board.push_back(cell.to_char());
+  }
+  return board;
+};
+
+auto Bitdoku::get_cell(const int index) const -> bit_field
+{
+  return bitboards[index].data;
+}
+
+auto Bitdoku::print_board() const -> void
+{
+  auto board = to_string();
+
+  fmt::print("{} ", board[0]);
+  for (int i = 0; i < board.size(); i++) {
+    if (i != 0) {
+      if ((i + 1) % 27 == 0) {
+        fmt::print("{}\n\n", board[i]);
+      } else if ((i + 1) % 9 == 0) {
+        fmt::print("{}\n", board[i]);
+      } else if ((i + 1) % 3 == 0) {
+        fmt::print("{}  ", board[i]);
+      } else if ((i + 1) % 27 == 0) {
+        fmt::print("{}  ", board[i]);
+      } else {
+        fmt::print("{} ", board[i]);
+      }
+    }
+  }
+}
+
+auto Bitdoku::get_row(const int row) const -> bit_field
+{
+  auto temp = Cell();
+  int start = row * 9;  // Start at the beginning of the row
+  for (int i = start; i < start + 9; ++i) {
+    if (bitboards[i].is_set()) {
+      temp |= bitboards[i];
+    }
+  }
+  return temp.data;
+}
+
+auto Bitdoku::get_col(const int index) const -> bit_field
+{
+  auto temp = Cell();
+  const auto stop = index + 73;
+  for (int i = index; i < stop;) {
+    if (bitboards[i].is_set()) {
+      temp |= bitboards[i];
+    }
+    i += 9;
+  }
+  return temp.data;
+}
+
+auto Bitdoku::get_block(const int block_index) const -> bit_field
+{
+  const int start_row = (block_index / 3) * 3;
+  const int start_col = (block_index % 3) * 3;
+  auto temp = Cell();
+  for (int row = start_row; row < start_row + 3; ++row) {
+    for (int col = start_col; col < start_col + 3; ++col) {
+      auto idx = index_row_col(row, col);
+      if (bitboards[idx].is_set()) {
+        temp |= bitboards[idx];
+      }
+    }
+  }
+  return temp.data;
+}
---
+++ source/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+
+#include <fmt/core.h>
+#include <lib.hpp>
+
+Bitdoku back_track(const std::string& board)
+{
+  Bitdoku sboard {board};
+  fmt::print("\ninitial board\n");
+  fmt::print("{}\n", board);
+  sboard.print_board();
+  for (int i = 0; i < flat_board_size; i++) {
+    sboard.set_possible(i);
+  }
+  auto solved = sboard.solve();
+  if (not(solved)) {
+    fmt::print("\nsolving error\n");
+  }
+
+  fmt::print("---------------------\n\n");
+  fmt::print("{}\n", sboard.to_string());
+  sboard.print_board();
+  return sboard;
+}
+
+void debug_board(const Bitdoku& sboard)
+{
+  for (int i = 0; i < 9; i++) {
+    if (sboard.is_valid_row(i)) {
+      fmt::print("row {} is valid\n", i);
+    }
+  }
+  for (int i = 0; i < 9; i++) {
+    if (sboard.is_valid_col(i)) {
+      fmt::print("col {} is valid\n", i);
+    }
+  }
+  for (int i = 0; i < 9; i++) {
+    if (sboard.is_valid_block(i)) {
+      fmt::print("block {} is valid\n", i);
+    }
+  }
+  if (sboard.is_valid()) {
+    fmt::print("board is valid\n");
+  }
+}
+
+// main function that takes a string representing a sudoku board
+// and solves it using the backtracking algorithm
+
+int main(int argc, char* argv[])
+{
+  const std::string board {
+      "381624957"
+      "694857213"
+      "527319684"
+      "269471835"
+      "148563792"
+      "753982461"
+      "415798326"
+      "846245179"
+      "972136548"};
+  const std::string board2 {
+      "001624007"
+      "690007013"
+      "500010080"
+      "069001800"
+      "040060790"
+      "050082400"
+      "010700026"
+      "806200079"
+      "002130008"};
+  const std::string board3 {
+      "000000020"
+      "105004006"
+      "000006900"
+      "604000070"
+      "020000080"
+      "503060000"
+      "040700100"
+      "200003040"
+      "007020003"};
+
+  const std::string board4 {
+      "0000000500006307100020003061000006000004000299087005000009070000150080"
+      "00000000067"};
+
+  if (argc != 2) {  // Expect exactly one additional argument: the board string
+    std::cerr << "Usage: " << argv[0] << " <sudoku_board_string>\n";
+    return 1;  // Return with error code
+  }
+
+  std::string board_in = argv[1];  // Get the board string from command line
+  if (board_in.length() != 81)
+  {  // Validate that the input string has exactly 81 characters
+    std::cerr << "Error: The Sudoku board must consist of exactly 81 digits "
+                 "(0-9).\n";
+    return 1;  // Return with error code
+  }
+
+  // Initialize the Sudoku board with the provided input
+  auto solvedBoard = back_track(board_in);
+  debug_board(
+      solvedBoard);  // Display the solved board and other debug information
+
+  return 0;  // Successful execution
+}
