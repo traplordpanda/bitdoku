8ef21afe48cf699dce89a92a73a79a427ba4a93c
Kyle AuBuchon <kdaub.tf2@gmail.com>
2024-11-29 20:11:32 -0500

    adding clang preset

--- source/gui_exe.cpp
+++ source/gui_exe.cpp
@@ -1,9 +1,20 @@
 import bitdoku;
 
 #include <bitdoku_gui.hpp>
-int main() {
-    auto solver = Bitdoku("0000000050201500300040070000100009200000008005087100"
-                          "00309000006000006008007209000");
+#include <string>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <board_string>\n";
+        return 1;
+    }
+    auto board_arg = std::string(argv[1]);
+    if (board_arg.size() != 81) {
+        std::cerr << "Board string must be 81 characters long\n";
+        return 1;
+    }
+    auto solver = Bitdoku(board_arg);
     SudokuVisualizer visualizer(540, solver);
     visualizer.run();
     return 0;
